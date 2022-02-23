35f9f219473b3cce702461a5441511b199d401fe
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-02-23 14:52:23 -0800

    Finish form practice

---
+++ script.js
@@ -0,0 +1,27 @@
+function checkPasswordMatch(password, confirmPassword){
+    return password == confirmPassword ? true : false;
+}
+
+function changeDisplay(element, isVisible){
+    isVisible ? element.style.display = "none" : element.style.display = "block";
+}
+
+function changeErrorVisibility(isValid){
+    const errorElement = this.parentElement.nextElementSibling;
+    changeDisplay(errorElement, isValid);
+}
+
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#c-password");
+
+confirmPassword.addEventListener("input", function(){
+    const password = document.querySelector("#password");
+    const isValid = checkPasswordMatch(password.value, this.value);
+    changeErrorVisibility.call(this, isValid);
+
+});
+password.addEventListener("input", function() {
+    changeErrorVisibility.call(this, this.validity.valid);
+    const event = new Event("input");
+    confirmPassword.dispatchEvent(event);
+});
