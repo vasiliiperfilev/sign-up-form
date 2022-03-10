27171fd8d7aa73ff2c9f2682d7d4812a82fda352
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-03-10 12:50:50 -0800

    Add JS validation

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-function checkPasswordMatch(password, confirmPassword){
-    return password == confirmPassword ? true : false;
+function checkPasswordsMatch() {
+  const password = document.querySelector('#password');
+  const confirmPassword = document.querySelector('#confPassword');
+  return password.value === confirmPassword.value;
 }
 
-function changeDisplay(element, isVisible){
-    isVisible ? element.style.maxHeight = "0px" : element.style.maxHeight = "200px";
+function showError(input) {
+  let isValid = false;
+  const errorElement = input.parentElement.nextElementSibling;
+  if (input.validity.valueMissing) {
+    errorElement.textContent = 'You need to enter a value';
+  } else if (input.validity.typeMismatch) {
+    errorElement.textContent = `Entered value needs to be ${input.type}`;
+  } else if (input.validity.tooShort) {
+    errorElement.textContent = `Value should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
+  } else if (input.id === 'confPassword' && !checkPasswordsMatch()) {
+    errorElement.textContent = 'Passwords must match';
+  } else if (input.id === 'password' && input.validity.patternMismatch) {
+    errorElement.textContent = '*Password must contain: at least 6 characters, at least 1 uppercase character, at least 1 lowercase character, at least 1 number';
+  } else if (input.validity.patternMismatch) {
+    errorElement.textContent = `Entered value needs to be ${input.type}`;
+  } else {
+    isValid = true;
+  }
+  errorElement.style.maxHeight = isValid ? '0px' : '200px';
 }
 
-function changeErrorVisibility(isValid){
+function addEventListenerToInput(input) {
+  input.addEventListener('input', function setupInput() {
     const errorElement = this.parentElement.nextElementSibling;
-    changeDisplay(errorElement, isValid);
+    if (this.validity.valid && checkPasswordsMatch()) {
+      errorElement.textContent = '';
+      return true;
+    }
+    showError(input);
+    return false;
+  });
 }
 
-const password = document.querySelector("#password");
-const confirmPassword = document.querySelector("#c-password");
+function doWithInput(form, func) {
+  let isDone = true;
+  const inputs = form.querySelectorAll('input');
+  const nodes = [...inputs];
+  nodes.forEach((input) => {
+    const result = func(input);
+    isDone = result && isDone;
+  });
+  return isDone;
+}
 
-confirmPassword.addEventListener("input", function(){
-    const password = document.querySelector("#password");
-    const isValid = checkPasswordMatch(password.value, this.value);
-    changeErrorVisibility.call(this, isValid);
-
+const form = document.querySelector('form');
+form.addEventListener('submit', function checkForm(event) {
+  if (!doWithInput(this, showError)) event.preventDefault();
 });
-password.addEventListener("input", function() {
-    changeErrorVisibility.call(this, this.validity.valid);
-    const event = new Event("input");
-    confirmPassword.dispatchEvent(event);
+doWithInput(form, addEventListenerToInput);
+// dispatch event for confirm password on password change
+document.querySelector('#password').addEventListener('input', () => {
+  const confirmPassword = document.querySelector('#confPassword');
+  showError(confirmPassword);
 });
